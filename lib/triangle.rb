685141f6be8ffe74374fa31e003df64acc78e143
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-07 21:41:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,7 @@ def initialize(length1, length2, length3)
   @length3 = length3
 end 
 def kind 
-  if (@length1 + @length2 > @length3) ||  (@length2 + @length 2> @length3) ||  (@length1 + @length2 > @length3) 
+  if (@length1 + @length2 > @length3) ||  (@length2 + @length3 > @length1) ||  (@length1 + @length2 > @length3) 
   elsif @length1 == @length2 == @length3 
     :equilateral
   elsif (@length1 == @length2) || (@length2 == @length3) || (@length3 == @length1) 
