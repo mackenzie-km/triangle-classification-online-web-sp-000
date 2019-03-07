23623cf2f7651d5cc7ee0e75ed701f02255503e8
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-07 21:47:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,6 +18,7 @@ def kind
   if (@length1 + @length2 < @length3) ||  (@length2 + @length3 < @length1) ||  (@length1 + @length3 < @length2) 
     raise TriangleError
   elsif (@length1 || @length2 || @length3) <= 0
+    raise TriangleError
   elsif (@length1 == @length2 && @length1 == @length3)
     :equilateral
   elsif (@length1 == @length2) || (@length2 == @length3) || (@length3 == @length1) 
