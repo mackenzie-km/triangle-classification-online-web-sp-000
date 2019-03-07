3cbb39509900096d6ed2b5daf0e5a597e83a38af
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-07 21:48:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ def initialize(length1, length2, length3)
 end 
 
 def kind 
-  if (@length1 + @length2 < @length3) ||  (@length2 + @length3 < @length1) ||  (@length1 + @length3 < @length2) 
+  if (@length1 + @length2 <= @length3) ||  (@length2 + @length3 <= @length1) ||  (@length1 + @length3 < @length2) 
     raise TriangleError
   elsif (@length1 || @length2 || @length3) <= 0
     raise TriangleError
