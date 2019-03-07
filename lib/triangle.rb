124d555ce2664a78f05ab9644cd233c97d583517
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-07 21:38:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,14 @@
 class Triangle
-  # write code here
+  attr_accessor :length1, :length2, :length3
+def initialize(length1, length2, length3)
+  @length1 = length1
+  @length2 = length2
+  @length3 = length3
+end 
+def kind 
+  if @length1 == @length2 == @length3 
+    :equilateral
+    
+  elsif 
+end 
 end
