2fcc35e2d9de2a2926fb884e12b30eeb81f5c24f
Mackenzie Moore <macmoore@ucdavis.edu>
2019-03-07 21:49:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,10 +13,15 @@ def initialize(length1, length2, length3)
   @length3 = length3
 end 
 
-def kind 
-  if (@length1 + @length2 <= @length3) ||  (@length2 + @length3 <= @length1) ||  (@length1 + @length3 <= @length2) 
-    raise TriangleError
+def valid?
+   if (@length1 + @length2 <= @length3) ||  (@length2 + @length3 <= @length1) ||  (@length1 + @length3 <= @length2) 
+    false
   elsif (@length1 || @length2 || @length3) <= 0
+    false
+end 
+
+def kind 
+  if valid? = false 
     raise TriangleError
   elsif (@length1 == @length2 && @length1 == @length3)
     :equilateral
